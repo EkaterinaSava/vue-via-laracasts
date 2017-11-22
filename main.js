55b557e5e88ab84c716a843d3f2192d38a3a4e0f
EkaterinaSava <ek@weare.lt>
2017-11-22 17:15:08 +0300

    ADD demo#6: The need for Computed properties

--- main.js
+++ main.js
@@ -65,4 +65,64 @@ var vm5 = new Vue({
       this.isLoading = true;
     }
   }
-})
+});
+
+/* –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+/* Step 06 — The Need for Computed Properties */
+/* –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+var vm6part1 = new Vue({
+  el: '#root-6-1',
+
+  data: {
+    message: 'Hello, Vue!'
+  },
+
+  computed: {
+    reversedMessage() {
+      return this.message.split('').reverse().join('');
+    }
+  }
+});
+
+var vm6part2 = new Vue({
+  el: '#root-6-2',
+
+  data: {
+    tasks: [
+      { description: 'Go to the store', completed: true },
+      { description: 'Finish screencast', completed: false },
+      { description: 'Make donation', completed: false },
+      { description: 'Clear inbox', completed: true },
+      { description: 'Make dinner', completed: false },
+      { description: 'Learn Vue', completed: false }
+    ]
+  },
+
+  computed: {
+    incompleteTasks() {
+      return this.tasks.filter( task => !task.completed );
+    }
+  }
+
+});
+
+/* –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+/* 05 — Attribute and Class Binding */
+/* –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+
+
+/* –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+/* 05 — Attribute and Class Binding */
+/* –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+
+
+
+/* –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+/* 05 — Attribute and Class Binding */
+/* –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+
+
+
+/* –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
+/* 05 — Attribute and Class Binding */
+/* –––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– */
